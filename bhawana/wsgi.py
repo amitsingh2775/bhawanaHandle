b7c427f23bf32ea79109bb5e7b7c289c5bfc8d47
amitsingh2775 <amitsinghbadram@gmail.com>
2025-09-30 13:34:44 +0530

    fixed

--- bhawana/wsgi.py
+++ bhawana/wsgi.py
@@ -1,9 +1,3 @@
-"""
-WSGI config for bhawana project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 
@@ -11,7 +5,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bhawana.settings')
 
 application = get_wsgi_application()
 
-# Expose aliases that Vercel expects
-# Vercel looks for `app` or `handler` in the module for serverless functions.
+# Vercel ke liye yeh line add karein
 app = application
-handler = application
