63a84b30d8a01b3dccb358ec2c16dec154869ee2
amitsingh2775 <amitsinghbadram@gmail.com>
2025-09-30 13:15:50 +0530

    fixed2

--- bhawana/settings.py
+++ bhawana/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os ### YEH ADD KAREIN ###
-import dotenv ### YEH ADD KAREIN ###
+import dotenv 
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+set -e
 
-pip3 install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
-python3.9 manage.py migrate
+# Install dependencies using the environment's Python
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
