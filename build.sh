b2278c4b15fab9cff209d997dc2bac22d8855a45
amitsingh2775 <amitsinghbadram@gmail.com>
2025-09-30 12:39:49 +0530

    fixed build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
+pip3 install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
+python3.9 manage.py migrate
