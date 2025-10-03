a55e06f021be4ea18eb5edb332c87e4a514024ab
amitsingh2775 <amitsinghbadram@gmail.com>
2025-10-03 14:43:49 +0530

    fixed

--- build.sh
+++ build.sh
@@ -3,13 +3,11 @@
 # Exit immediately if a command fails
 set -e
 
-# Install dependencies using python's module
+# Install project dependencies
 python -m pip install -r requirements.txt
 
 # Run database migrations
 python manage.py migrate --no-input
 
 # Collect static files
-python manage.py collectstatic --no-input
-
-echo "Build finished!"
+python manage.py collectstatic --no-inputv
